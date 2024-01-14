653bde92e5e289315454e516804ebd172c645e80
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-14 20:06:46 -0300

    Service layer, component  registration

--- src/main/java/com/giuliano/course/repositories/UserRepository.java
+++ src/main/java/com/giuliano/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.giuliano.course.repositories;
 import com.giuliano.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
