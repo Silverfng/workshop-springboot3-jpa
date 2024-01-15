da4bb5caa6d51c4a025e108d1a1d60476a5fad62
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-15 14:21:02 -0300

    OrderStatus Enum

--- src/main/java/com/giuliano/course/entities/Order.java
+++ src/main/java/com/giuliano/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.giuliano.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.giuliano.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -23,9 +25,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -52,6 +55,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
