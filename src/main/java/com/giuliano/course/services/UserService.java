ee6392c2ccc1004a23dd4028e7cd897af91e4d91
Ilson Giuliano <ilsongiuliano@yahoo.com.br>
2024-01-19 13:11:35 -0300

    Exception handling - findyById

--- src/main/java/com/giuliano/course/services/UserService.java
+++ src/main/java/com/giuliano/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.giuliano.course.services;
 
 import com.giuliano.course.entities.User;
 import com.giuliano.course.repositories.UserRepository;
+import com.giuliano.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
